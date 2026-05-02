ce0464b3c306d9d5b79ddfd6f5f8a0e9f92f4067
hu900 <90720482+hu900@users.noreply.github.com>
2026-05-02 17:28:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,12 @@
-TARGET_URL = "https://webook.com/ar/"
-MAX_CONCURRENT_JOBS = 2
+import os
+
+TARGET_URL = os.getenv("TARGET_URL", "https://webook.com/ar/events/YOUR_EVENT_SLUG")
+
+ALLOWED_CATEGORIES = [
+    "C5", "C7", "C8", "C4", "C3 RIGHT", "C3 LEFT",
+    "C2 RIGHT", "C2 LEFT", "C1 RIGHT", "C1 LEFT",
+    "BRONZE", "BRONZE 1", "BRONZE 2"
+]
+
+MAX_HOLDS = int(os.getenv("MAX_HOLDS", "5"))
+DEBUG_MODE = os.getenv("DEBUG_MODE", "true").lower() == "true"
